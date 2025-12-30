06a57f395dd2cfe41f33232b5edc746635aa1303
kynoratech <kynoratech1@gmail.com>
2025-12-30 12:13:29 -0300

    Add WebPay

--- app/api/webpay/commit-transaction/route.ts
+++ app/api/webpay/commit-transaction/route.ts
@@ -1,28 +1,26 @@
-"use client";
+import { NextResponse } from "next/server";
+import { WebpayPlus, Environment, IntegrationApiKeys } from "transbank-sdk";
 
-import { WebpayPlus } from "transbank-sdk";
+export const dynamic = "force-dynamic";
 
 export async function POST(req: Request) {
   try {
     const { token_ws } = await req.json();
     if (!token_ws) {
-      return Response.json({ ok: false, error: "Falta token_ws" }, { status: 400 });
+      return NextResponse.json({ ok: false, error: "Falta token_ws" }, { status: 400 });
     }
 
     const tx = new WebpayPlus.Transaction({
       commerceCode: "597055555532",
-      apiKey: "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C",
-      environment: "TEST",
+      apiKey: IntegrationApiKeys.WEBPAY,
+      environment: Environment.Integration,
     });
 
     const response = await tx.commit(token_ws);
 
-    return Response.json({ ok: true, response });
+    return NextResponse.json({ ok: true, response });
 
   } catch (err: any) {
-    return Response.json(
-      { ok: false, error: err.message ?? "Error al confirmar pago" },
-      { status: 500 }
-    );
+    return NextResponse.json({ ok: false, error: err.message }, { status: 500 });
   }
 }
